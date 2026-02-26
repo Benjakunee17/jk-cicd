abad6530668f811a99dc78d505a8e19fe0f6d0a2
Benja Kuneepong <benja.kunee@gmail.com>
2026-02-26 08:23:08 +0700

    ec2 instance

---
+++ terraform/variable.tf
@@ -0,0 +1,25 @@
+/**
+ * Create by : Benja kuneepong
+ * Date : Wed, Aug  7, 2024 10:45:12 AM
+ * Purpose : ประกาศตัวแปลเริิ่มต้นเพื่อไปใช้ในไฟล์​ var ของแต่ละ environment
+ */
+
+
+
+variable "aws_region" {}
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "environment" {}
+variable "create_by_name" {}
+
+variable "vpc_id" {}
+variable "subnet_a" {}
+variable "subnet_b" {}
+variable "subnet_c" {}
+variable "subnet_d" {}
+
+variable "ec2_instance_image" {}
+variable "ec2_instance_type" {}
+
+
